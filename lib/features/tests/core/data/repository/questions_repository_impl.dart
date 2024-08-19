03e68e55d7649f0d9a71399fb02cf77d73da056c
oluwasegun <ilorioluwasegun@gmail.com>
2024-08-19 14:46:24 -0400

    Add remote option for loading json file

--- lib/features/tests/core/data/repository/questions_repository_impl.dart
+++ lib/features/tests/core/data/repository/questions_repository_impl.dart
@@ -5,6 +5,7 @@ import 'package:dri_learn/features/tests/core/domain/model/question_model.dart';
 import 'package:dri_learn/features/tests/core/domain/model/test_type.dart';
 import 'package:dri_learn/features/tests/core/domain/repository/questions_repository.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:http/http.dart' as http;
 
 const questionLength = 40;
 const knowSignLength = 20;
@@ -13,8 +14,12 @@ const singleKnowSignLength = 20;
 class QuestionsRepositoryImpl extends QuestionsRepository {
   @override
   Future<List<QuestionModel>> getQuestions({TestType? type}) async {
-    final String response =
-        await rootBundle.loadString('assets/data/ontario_test_questions.json');
+    String response = "";
+    final remoteResponse = await http
+        .get(Uri.parse('https://drilearn.web.app/ontario_test_questions.json'));
+    if (remoteResponse.statusCode == 200) {
+      response = remoteResponse.body;
+    } 
     final List<dynamic> data = await json.decode(response);
     List<QuestionModel> dataList =
         data.map((json) => QuestionModel.fromJson(json)).toList();
